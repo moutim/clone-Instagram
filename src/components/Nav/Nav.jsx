48dde021a6d7eb8af02020d2cdfeb0acfb13d265
Vitor Moutim <moutimg@gmail.com>
2023-11-28 01:00:29 -0300

    feat: insere todos os icones do header e nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import logo from '../../images/instagram-logo.svg';
 import './nav.css';
 import { GoHomeFill } from 'react-icons/go';
+import { IoSearchOutline } from 'react-icons/io5';
+import { MdOutlineExplore } from 'react-icons/md';
+import { BiMoviePlay } from 'react-icons/bi';
+import { PiPaperPlaneTilt } from 'react-icons/pi';
+import { FaRegHeart } from 'react-icons/fa';
+import { FiPlusSquare } from 'react-icons/fi';
+import { BsThreads } from 'react-icons/bs';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import user from '../../images/user.svg';
 import NavIcon from '../NavIcon/NavIcon';
 
 function Nav() {
@@ -12,7 +21,19 @@ function Nav() {
       </div>
 
       <div>
-        <NavIcon img={ <GoHomeFill /> } text='Página inicial'/>
+        <NavIcon img={ <GoHomeFill /> } text='Página inicial' />
+        <NavIcon img={ <IoSearchOutline /> } text='Pesquisa' />
+        <NavIcon img={ <MdOutlineExplore /> } text='Explorar' />
+        <NavIcon img={ <BiMoviePlay /> } text='Reels' />
+        <NavIcon img={ <PiPaperPlaneTilt /> } text='Mensagens' />
+        <NavIcon img={ <FaRegHeart /> } text='Notificações' />
+        <NavIcon img={ <FiPlusSquare /> } text='Criar' />
+        <NavIcon img={ <img src={ user } alt="Profile picture" /> } text='Perfil' />
+      </div>
+
+      <div>
+        <NavIcon img={ <BsThreads /> } text='Threads' />
+        <NavIcon img={ <RxHamburgerMenu /> } text='Mais' />
       </div>
     </header>
   );
